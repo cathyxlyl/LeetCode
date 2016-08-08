ac1ed68f16a77cc194040af7f3c8e6fa231ef9f2
Cathy Wang <lucianlcp@126.com>
2016-08-08 14:28:59 +0800

    Create 1_Two_Sum.py

---
+++ Python/1_Two_Sum.py
@@ -0,0 +1,28 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+UPDATE (2016/2/13):
+The return format had been changed to zero-based indices. Please read the above updated description carefully.
+*/
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(0, n - 1):
+            for j in range(i + 1, n):
+                if (nums[i] + nums[j] == target):
+                    return [i + 1, j + 1]
+        return [0, 0]
